35999d2209177b240acae5e779cea055472d245c
226252 <piotrekklim1996@gmail.com>
2017-03-07 10:18:05 +0100

    uporzadkowanie kodu

--- 1_lista/1_obiektowo.cpp
+++ 1_lista/1_obiektowo.cpp
@@ -8,34 +8,43 @@ struct tab
 	int x;
 	int y;
 	int** tablica;
-	tab(int X, int Y)
-	{
-		x=X;
-		y=Y;
-		tablica = new int * [x];
-		for(int i=0; i<x; i++)
-			tablica[i] = new int[y];
-	}
+	// metody, konstruktory
+	tab(int X, int Y);
+	void wypelnij();
+	void wyswietl();
+	int znajdz_najw();
+	void usun();
+};
 
-	void wypelnij_tablice()
-	{
+tab::tab(int X, int Y)
+{
+	x=X;
+	y=Y;
+	tablica = new int * [x];
 	for(int i=0; i<x; i++)
+		tablica[i] = new int[y];
+}
+
+void tab::wypelnij()
+{
+	for(int i=0; i<x; i++){
 			for(int j=0; j<y; j++)
 				tablica[i][j]=rand() % 10;
 	}
+}
 
-	void wyswietl_tablice()
-	{
+void tab::wyswietl()
+{
 	for(int i=0; i<x; i++){
 		for(int j=0; j<y; j++){
 			cout << tablica[i][j]<<" ";
 		}
 		cout << endl;
 	}
-	}
+}
 
-	int znajdz_najw()
-	{
+int tab::znajdz_najw()
+{
 	int tmp;
 	tmp=tablica[0][0];
 	for(int i=0; i<x; i++)
@@ -43,22 +52,13 @@ struct tab
 			if (tablica[i][j]>tmp)
 				tmp=tablica[i][j];
 	return tmp;
-	}
+}
 
-	void usun_tablice()
-	{
+void tab::usun()
+{
 	for(int i=0; i<x; i++)
 		delete [] tablica[i];
-	} 
-
-};
-
-
-
-
-
-
-
+}
 
 
 
@@ -70,9 +70,10 @@ struct tab
 		cout<<"y";
 		cin>> y;
 		tab tablica(x, y);
-		tablica.wypelnij_tablice();
-		tablica.wyswietl_tablice();
+		tablica.wypelnij();
+		tablica.wyswietl();
 		cout << "Biggest: " << tablica.znajdz_najw() << endl;
+		tablica.usun();
 
 	}
 
