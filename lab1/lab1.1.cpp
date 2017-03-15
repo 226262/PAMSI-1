b78cff166f41de70673593bd567a3aac1d54553b
226252 <piotrekklim1996@gmail.com>
2017-03-15 12:33:36 +0100

    Dodanie komentarzy oraz screenow z pomiarami

--- lab1/lab1.1.cpp
+++ lab1/lab1.1.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 using namespace std;
 #include <cstdlib>
-#include <chrono>
-#include <ctime>
 #include <time.h>
 
-class tab
+class tab              //
 {
-	int length;
-	double * tablica;
-public:
-	void enlarge(int);
+	int length;        //dlugosc tblicy
+	double * tablica;  //dynamiczna tablica
+	void enlarge(int); //funkcja powiekszania, przyjmowany argument mowi do ilu ziwekszyc tablice
 public:
 	//konstruktory, metody 
 	tab(int);
 	~tab();
-	void input();
-	void fill_rand(int);
-	void output();
-	int size();
-	void size_out();	
-	friend void measures(int, int);
-	double mean();
+	void input();      //metoda sluzaca do wpisywania z klawiatury elementow tablicy
+	void fill_rand(int);//wypelnia tablice randomowymi liczbami, argument mowi jaki jest zakres randomowych liczb
+	void output();      //wypisuje wartosci tablicy
+	int size();         //zwraca rozmiar tablicy
+	void size_out();	//wypisuje rozmiar tablicy
+	friend void measures(int, int); //zaprzyjazniona funkcja sluzaca do pomiarow
+	double mean(); //funkcja zwracajaca srednia wszystkich elementow tablicy
 	//przeciazenia
-	double & operator[](int element) {return tablica[element];}
+	double & operator[](int element) {return tablica[element];} //przeciazenie operatora [] 
 };
 // 
 tab::tab(int x){
@@ -83,7 +80,7 @@ double tab::mean(){
 	return x/length;
 }
 // 
-struct clock_
+class clock_
 {
 	double start, end;
 	double duration_; 
@@ -104,56 +101,54 @@ struct clock_
 		return duration_;
 	}
 	//
-	friend void measures(int);
+	friend void measures(int, int);
 };
 //
 
-void measures(int x, int y){ //x rozmiar badanej tablicy, y ilosc powtorzen mierzenia czasu
+void measures(int x, int y){ //funckja wykonujaca pomiary czasu, x rozmiar badanej tablicy, y ilosc powtorzen mierzenia czasu
 	clock_ t;
-	tab oryginal(x);
-	tab pomiar(x);
-	oryginal.fill_rand(100);
-	tab wyniki_add1(y);
-	tab wyniki_x2(y);
+	tab oryginal(x); //inicjalizacja tablicy oryginalnej 
+	tab pomiar(x);   //inicjalizacja tablicy na ktorej wykonane zzostana pomiary
+	oryginal.fill_rand(100); //wypelnienie tablicy oryginalnej randomowymi wartosciami od 0 do 100
+	tab wyniki_add1(y); //inicjalizacja tablicy do ktorej zapisane zostana pomiary dla powiekszania o 1
+	tab wyniki_x2(y);   //inicjalizacja tablicy do ktorej zapisane zostana pomiary dla powiekszania razy dwa
 	for(int i=0; i<y; i++){
-		pomiar=oryginal;
-		t.begin();
-		pomiar.enlarge(pomiar.length+1);
-		t.stop();
-		wyniki_add1[i]=t.duration();
+		pomiar=oryginal; //tablica pomiarowa jest taka sama jak oryginal
+		t.begin();       //rozpoczecie mierzenia czasu
+		pomiar.enlarge((pomiar.length)+1); //powiekszenie tablicy o 1 
+		t.stop();        //koniec pomiaru czasu
+		wyniki_add1[i]=t.duration(); //wpisanie wyniku do tablicy
 	}
 	for(int i=0; i<20; i++){
 		pomiar=oryginal;
 		t.begin();
-		pomiar.enlarge(2*(pomiar.length));
+		pomiar.enlarge(2*(pomiar.length)); //powiekszenie tablicy dwukrotnie
 		t.stop();
 		wyniki_x2[i]=t.duration();
-		// cout<< t.duration();
 	}
 
-	cout<< "wyniki dla " << y << " elementow." << endl;
+	cout<< "wyniki dla " << x << " elementow." << endl;
 	cout<< "+1" << endl; 
 	wyniki_add1.output();
 	cout<< "srednia: " << wyniki_add1.mean();
+	cout << endl;
 	cout<< "x2" << endl; 
 	wyniki_x2.output();
 	cout<< "srednia: " << wyniki_x2.mean();
+	cout << endl;
 }
 // 
 int main(){
 	double x;
-	cin >> x;
 	int y;
+	cout << "ilosc elementow tablicy: ";
+	cin >> x;
+	cout << "ilosc powtorzen obliczen: ";
 	cin >> y;
+	cout << "*************" << endl;
 	measures(x, y);
-
 	return 0;
 }
 
-// std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-// std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
-
-// std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
-// std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() <<std::endl;
 
  
